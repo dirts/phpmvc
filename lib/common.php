b05f85baeb0578188497ed1e1a5662cd91c7fa73
dirts <nickey.song@163.com>
2013-11-25 15:44:01 +0800

    common

--- lib/common.php
+++ lib/common.php
@@ -1,4 +1,34 @@
 <?php
+
+#模块加载器
+function load($file){
+	require($file);
+}
+
+#配置函数
+function config($name, $value = ''){
+	static $_config = array();
+	if(is_array($name)){
+		$_config = array_merge($_config, $name);
+		return $_config;
+	}else if(is_string($name)){
+	
+		$argc = func_num_args();
+		if($argc == 1){
+			$_tmp = $_config;
+			$keys = explode('.', $name);
+			foreach($keys as $k){
+				if(!isset($_tmp[$k])) break;
+				$_tmp = $_tmp[$k];
+			}
+			return $_tmp;
+		}elseif($argc == 2){
+			return $_config[$name] = $value;
+		}
+		return $_config;
+	}
+
+}
 	
 function U($app , $param=null){
 	$app = explode('/', $app);
@@ -45,15 +75,9 @@ function getIP() {
 }
 
 function preg_test($reg, $str){
-
 	return preg_match($reg, $str);
 }
 
-function console_log($msg){
-	echo '<pre>';
-	var_dump($msg);
-	echo '</pre>';
-}
 
 function array_walk_strim(&$item, $key, $sp){
 	$item = "$sp$item$sp";
@@ -64,26 +88,34 @@ function get_post($key, $value){
 	return $return;
 }
 
-#配置函数
-function config($name, $value = ''){
-	static $_config = array();
-	if(is_array($name)){
-		$_config = array_merge($_config, $name);
-		return $_config;
-	}else if(is_string($name)){
-	
-		$argc = func_num_args();
-		if($argc == 1){
-			$_tmp = $_config;
-			$keys = explode('.', $name);
-			foreach($keys as $k)
-				$_tmp = $_tmp[$k];
-			return $_tmp;
-		}elseif($argc == 2){
-			return $_config[$name] = $value;
-		}
-		return $_config;
-	}
+#
+function http($url){
 
+		$options = array( 
+			CURLOPT_RETURNTRANSFER => true,         // return web page 
+			CURLOPT_HEADER         => false,        // don't return headers 
+			CURLOPT_FOLLOWLOCATION => true,         // follow redirects 
+			CURLOPT_ENCODING       => "",           // handle all encodings 
+			CURLOPT_USERAGENT      => "spider",     // who am i 
+			CURLOPT_AUTOREFERER    => true,         // set referer on redirect 
+			CURLOPT_CONNECTTIMEOUT => 120,          // timeout on connect 
+			CURLOPT_TIMEOUT        => 240,          // timeout on response 
+			CURLOPT_MAXREDIRS      => 10,           // stop after 10 redirects 
+			CURLOPT_POST           => 1,            // i am sending post data 
+			CURLOPT_POSTFIELDS     => '',    // this are my post vars 
+			CURLOPT_SSL_VERIFYHOST => 0,            // don't verify ssl 
+			CURLOPT_SSL_VERIFYPEER => false,        // 
+			CURLOPT_VERBOSE        => 1                // 
+		); 
+
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, $url);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, array());
+	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); 
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); 
+
+	$respons = curl_exec($curl);
+	return $respons;
 }
+
 ?>
